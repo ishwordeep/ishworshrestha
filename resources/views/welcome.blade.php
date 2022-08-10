71b7827a4ca7446b0be5201775fcdcbbfef77206
ishwordeep <ishwor055@gmail.com>
2022-08-10 15:14:42 +0545

    tab icon added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <link rel="icon" src=> --}}
+    <link rel = "icon" type = "image/png" href = "{{asset('images/id.png')}}" class="">
     <title>IshworDeep</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
-        img,
         button {
-            opacity: .75;
+            opacity: .5;
             transition: opacity .5s ease-in-out;
         }
-
-        img:hover,
         button:hover {
             opacity: 1;
         }
