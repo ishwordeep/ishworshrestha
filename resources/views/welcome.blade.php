6e26d2776fd354c1261f45b2aaa6eeed1727a01e
ishwordeep <ishwor055@gmail.com>
2022-08-10 16:45:48 +0545

    linked in added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
+        html {
+            scroll-behavior: smooth;
+        }
+
         body {
             background: #1B2430;
         }
@@ -118,6 +122,7 @@
             <a href="https://www.facebook.com/ishwordeep"><button class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa-brands fa-facebook"></i></button></a>
             <a href="https://www.instagram.com/ishwordeep/"><button class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa-brands fa-instagram"></i></button></a>
             <a href="https://github.com/ishwordeep"><button class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Github"><i class="fa-brands fa-github"></i></button></a>
+            <a href="https://www.linkedin.com/in/ishwordeep/"><button class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i class="fa-brands fa-linkedin"></i></button></a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
