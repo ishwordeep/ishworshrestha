27f2c95874beab471eead89ad9fcbbab969130f0
ishwordeep <ishwor055@gmail.com>
2022-08-10 15:31:50 +0545

    email added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
+        .scrollbar-hidden::-webkit-scrollbar {
+  display: none;
+}
         button {
             opacity: .5;
             transition: opacity .5s ease-in-out;
@@ -96,6 +99,13 @@
     </style>
 </head>
 <body class="bg-dark text-light ">
+    <div class="container ">
+        <div class="row">
+            <div class="col-12 flex-end text-light d-flex justify-content-end mt-2">
+                <p><i class="fa-solid fa-envelope px-3"></i> ishwor055@gmail.com</p>
+            </div>
+        </div>
+    </div>
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
         <div style="width: 150px;">
             <img src="{{asset('images/id.png')}}" alt="Ishwor deep" class="swing-in-top-fwd img-thumbnail rounded-circle ">
